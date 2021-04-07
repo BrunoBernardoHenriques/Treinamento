214a7281184785328c1bb0f7c904bcc77c20c947
BrunoBernardoHenriques <51165192+BrunoBernardoHenriques@users.noreply.github.com>
2021-04-07 10:47:44 -0300

    Add files via upload
    ReadLine

---
+++ WriteLine and ReadLine/WriteLine and ReadLine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WriteLine_and_ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adicionando Variável nomePessoa
+            string nomePessoa = "Bruno";
+
+            /*
+           Adicionando Variável idadePessoa
+             Adicionando Variável salárioPessoa
+             */
+
+            int idadePessoa = 18;
+            double salarioPessoa = 1565.54;
+
+            //Mostrar o Console para digitar o nome
+            Console.WriteLine("Digite seu nome");
+
+            //ReadLine
+            nomePessoa = Console.ReadLine();
+
+            //Mostrando Valores na Tela
+            Console.WriteLine("Nome: "+nomePessoa);
+            Console.WriteLine("Idade: " + idadePessoa);
+            Console.WriteLine("Salário: " + salarioPessoa);
+
+
+        }
+    }
+}

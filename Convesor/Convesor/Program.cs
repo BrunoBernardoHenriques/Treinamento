9bb1f9de3261165f1e0b9d48f760fe0a40f9c7b5
BrunoBernardoHenriques <51165192+BrunoBernardoHenriques@users.noreply.github.com>
2021-04-08 11:00:29 -0300

    Add files via upload

---
+++ Convesor/Convesor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Convesor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome");
+            string nome = null;
+            int idade = 0;
+            double salario = 0;
+
+            //Adicionando o Nome na Variável
+            nome = Console.ReadLine();
+
+
+      
+            Console.WriteLine("Digite sua idade");
+            //Adicionando a idade na Variável
+            idade = int.Parse(Console.ReadLine());
+            salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("Seu nome é: " + nome);
+            Console.WriteLine("Sua idade é: " + idade);
+            Console.WriteLine("Seu salário é: " + salario);
+        }
+    }
+}

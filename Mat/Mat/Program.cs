6fa854c0e0c97b658cf4381e1ea1593ec81b97ce
Bruno Bernardo Henriques Cardoso <51165192+BrunoBernardoHenriques@users.noreply.github.com>
2021-04-08 11:46:58 -0300

    Add files via upload

---
+++ Mat/Mat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Mat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor1, valor2;
+            valor1 = 28;
+            valor2 = 2;
+
+            double soma = valor1 + valor2;
+
+            double multiplica = valor1 * valor2;
+
+            double dividir = valor1 / valor2;
+
+            double subtrair = valor1 - valor2;
+
+            double media = soma / 2;
+            Console.WriteLine("A soma de: "+valor1 +" + "+valor2+" = "+soma);
+
+            Console.WriteLine("A multiplicação de: " + valor1 + " . " + valor2 + " = " + multiplica);
+
+            Console.WriteLine("A divisão de: " + valor1 + " / " + valor2 + " = " + dividir);
+
+            Console.WriteLine("A subtração de: " + valor1 + " - " + valor2 + " = " + subtrair);
+
+            Console.WriteLine("A média de: " + valor1 + " e " + valor2 + " = " + media);
+        }
+    }
+}

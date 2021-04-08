e2c0810f5297d5e2761ef8ae15efbe2a407de1ac
Bruno Bernardo Henriques Cardoso <51165192+BrunoBernardoHenriques@users.noreply.github.com>
2021-04-08 13:55:18 -0300

    Add files via upload

---
+++ If Else/If Else/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double numero = 0;
+
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um número");
+
+             //Testar se número é par 
+            double parouimpar = numero % 2;
+
+            if(parouimpar != 0)
+            {
+                Console.WriteLine(numero + " é Impar");
+            }
+            else if(parouimpar == 0)
+            {
+                Console.WriteLine(numero + " é Par");
+            }
+          
+
+
+        }
+    }
+}

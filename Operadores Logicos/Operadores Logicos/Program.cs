39ba12f2006ba2d092c307718164070d348bf174
Bruno Bernardo Henriques Cardoso <51165192+BrunoBernardoHenriques@users.noreply.github.com>
2021-04-08 16:47:58 -0300

    Add files via upload

---
+++ Operadores Logicos/Operadores Logicos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Operadores_Logicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+
+            Console.WriteLine("Digite um nome");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade");
+            idade = int.Parse(Console.ReadLine());
+            // 11
+           if(nome == "Bruno" && idade == 18)
+            {
+                Console.WriteLine("Seu nome é "+nome+" e sua idade é "+idade+" anos");
+
+            }
+            if (nome == "Bruno" || idade == 18)
+            {
+                Console.WriteLine("Seu nome pode ser Bruno ou sua idade pode ser 18 anos");
+
+            }
+
+            bool falso = true;
+            bool verdade = false;
+
+            if(!falso)
+            {
+                Console.WriteLine("Falso é verdade");
+            }
+            if (!verdade)
+            {
+                Console.WriteLine("Verdade é verdade");
+            }
+
+        }
+    }
+}
